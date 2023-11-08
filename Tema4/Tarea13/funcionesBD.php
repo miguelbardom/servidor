5fa93d42213e52b27ee966c21e8b5a2dc0ab11cb
Miguel <miguelbarbadguez@gmail.com>
2023-11-08 15:47:46 +0000

    navidad

--- Tema4/Tarea13/funcionesBD.php
+++ Tema4/Tarea13/funcionesBD.php
@@ -39,6 +39,51 @@ function crearBD(){
 
 }
 
+// READ
+
+$con = new mysqli();
+try {
+    $con->connect(IP, USER, PASS, 'rios');
+    $script = '';
+    $con->multi_query($script);
+    do {
+        $con->store_result();
+        if (!$con->next_result()) {
+            break;
+        }
+    } while (true);
+
+    // $sql = 'update alumnos set nombre = ?, edad = ? where id = ?';
+    // $stmt = $con->stmt_init();
+    // $stmt->prepare($sql);
+    // $nombre = 'Raul'; $edad = 35; $id = 3;
+    // $stmt->bind_param('sii', $nombre, $edad, $id);
+    // $stmt->execute();
+    $con->close();
+
+} catch (\Throwable $th) {
+    //throw $th;
+    switch ($th->getCode()) {
+        case '1062':
+            echo "Ha introducido el mismo id";
+            break;
+        case '1045':
+            echo 'Usuario incorrecto, Password incorrecto o IP incorrecta, aunque exista';
+            break;
+        case '2002':
+            echo 'IP incorrecta y distinta';
+            break;
+        case '1049':
+            echo 'BD incorrecta';
+            break;
+        default:
+            echo $th->getMessage();
+            break;
+    }
+    echo "Error";//error de dns
+    $con->close();
+}
+
 
 
 /*
@@ -86,51 +131,6 @@ try {
 }
 */
 
-// READ
-/*
-$con = new mysqli();
-try {
-    $con->connect(IP, USER, PASS, 'rios');
-    $script = '';
-    $con->multi_query($script);
-    do {
-        $con->store_result();
-        if (!$con->next_result()) {
-            break;
-        }
-    } while (true);
-
-    // $sql = 'update alumnos set nombre = ?, edad = ? where id = ?';
-    // $stmt = $con->stmt_init();
-    // $stmt->prepare($sql);
-    // $nombre = 'Raul'; $edad = 35; $id = 3;
-    // $stmt->bind_param('sii', $nombre, $edad, $id);
-    // $stmt->execute();
-    $con->close();
-
-} catch (\Throwable $th) {
-    //throw $th;
-    switch ($th->getCode()) {
-        case '1062':
-            echo "Ha introducido el mismo id";
-            break;
-        case '1045':
-            echo 'Usuario incorrecto, Password incorrecto o IP incorrecta, aunque exista';
-            break;
-        case '2002':
-            echo 'IP incorrecta y distinta';
-            break;
-        case '1049':
-            echo 'BD incorrecta';
-            break;
-        default:
-            echo $th->getMessage();
-            break;
-    }
-    echo "Error";//error de dns
-    $con->close();
-}
-*/
 
 //UPDATE
 /*
