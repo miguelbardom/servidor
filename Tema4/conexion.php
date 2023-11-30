a1ba2e2e761f1d75444ba00e5489311b92f30400
Miguel <miguelbarbadguez@gmail.com>
2023-11-30 19:38:44 +0000

    clase 30/11

--- Tema4/conexion.php
+++ Tema4/conexion.php
@@ -1,5 +1,5 @@
 <?
-
+/*
 require('./confBD.php');
 
 try {
@@ -14,7 +14,7 @@ try {
     mysqli_stmt_bind_param($stmt, 'si', $rnombre, $redad);
     mysqli_stmt_execute($stmt);
 
-    mysqli_query($con,$sql);
+    //mysqli_query($con,$sql);
 
     mysqli_close($con);
 } catch (\Throwable $th) {
@@ -32,6 +32,7 @@ try {
     echo mysqli_connect_errno();
     echo mysqli_connect_error();
 }
+*/
 
 /*
 Posibles errores:
@@ -42,3 +43,30 @@ Posibles errores:
 - BD incorrecta: Error 1049 Unknown database 'pruebas'
 */
 
+require('./confBD.php');
+
+try {
+    $con = mysqli_connect(IP, USER, PASS, 'prueba');
+    $sql = 'delete from alumnos where id>=5';
+    $result = mysqli_query($con,$sql);
+    echo mysqli_affected_rows($con);
+    while($array = mysqli_fetch_assoc($result)){
+        echo "<pre>";
+        print_r($array);
+    }
+    mysqli_close($con);
+} catch (\Throwable $th) {
+    switch ($th->getCode()) {
+        case '1062':
+            echo "Ha introducido el mismo id";
+            break;
+        
+        default:
+            echo $th->getMessage();
+            break;
+    }
+    echo "Error";//error de dns
+    mysqli_close($con);
+    echo mysqli_connect_errno();
+    echo mysqli_connect_error();
+}
